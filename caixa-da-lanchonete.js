a0cd4ae181ec6d5b3a5a7a1cbeebf97ee20fa4bf
isaaregina <isabellaregina344@gmail.com>
2023-08-16 15:43:33 -0300

    feat: m

--- caixa-da-lanchonete.js
+++ caixa-da-lanchonete.js
@@ -17,4 +17,32 @@ class CaixaDaLanchonete {
       credito: 1.03, // 3% de acréscimo
     };
   }
+
+  calcularValorDaCompra(formaDePagamento, itens) {
+    if (!this.formasDePagamento[formaDePagamento]) {
+      return 'Forma de pagamento inválida!';
+    }
+
+    if (itens.length === 0) {
+      return 'Não há itens no carrinho de compra!';
+    }
+
+    let total = 0;
+
+    for (const itemInfo of itens) {
+      const [codigo, quantidade] = itemInfo.split(',');
+
+      if (!this.cardapio[codigo]) {
+        return 'Item inválido!';
+      }
+
+      const precoItem = this.cardapio[codigo];
+      total += precoItem * quantidade;
+    }
+
+    const formaDePagamentoInfo = this.formasDePagamento[formaDePagamento];
+    total *= formaDePagamentoInfo;
+
+    return `R$ ${total.toFixed(2).replace('.', ',')}`;
+  }
 }
